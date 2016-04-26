ba15cf6ebd18fb17aa4da40ab043ec6450516299
Tomas Korcak <korczis@gmail.com>
2016-04-26 21:04:37 +0200

    Version 0.0.2

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-var PEG = require("peg");
+var PEG = require("pegjs");
 
 /*
  MIT License http://www.opensource.org/licenses/mit-license.php
  Author Tomas Korcak <korczis@gmail.com>
  */
-module.exports = function(content) {
+module.exports = function (content) {
     this.cacheable && this.cacheable();
     this.value = content;
-    return "module.exports = " + PEG.buildParser(JSON.stringify(content));
+
+    const input = content;
+    const output = PEG.buildParser(content.toString(), {
+        output: "source"
+    });
+
+    return "module.exports = " + output;
 }
